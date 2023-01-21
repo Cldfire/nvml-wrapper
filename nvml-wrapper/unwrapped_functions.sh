81f752ea74b5a6f03f5f98e8f4d276efab62c02b
Jarek Samic <jarek.samic@cldfire.dev>
2023-01-21 14:01:53 -0500

    Add script to find unwrapped functions

---
+++ nvml-wrapper/unwrapped_functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Writes all unwrapped function names to `unwrapped_functions.txt`. This can
+# help discover functions to work on wrapping.
+#
+# `ripgrep` must be installed and available. `cargo install ripgrep`
+
+all_functions=$(rg 'pub unsafe fn (\w+)' -oNr '$1' ../nvml-wrapper-sys/src/bindings.rs)
+readarray -t all_functions_arr <<< "$all_functions"
+
+output=""
+
+for name in "${all_functions_arr[@]}"
+do
+    if [[ $name = "new" ]]
+    then
+        continue
+    fi
+
+    # filter out function names that appear in the wrapper source
+    if ! rg ".lib.${name}." -q src/* ;
+    then
+        output+="${name}"
+        output+=$'\n'
+    fi
+done
+
+echo "$output" > unwrapped_functions.txt
